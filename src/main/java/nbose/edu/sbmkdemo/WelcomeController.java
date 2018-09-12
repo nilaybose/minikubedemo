e646275020f9ad28bf237aa980894e820bcef469
Nilay K Bose <nilay.bose@gmail.com>
2018-09-12 02:50:37 -0400

    test

--- src/main/java/nbose/edu/sbmkdemo/WelcomeController.java
+++ src/main/java/nbose/edu/sbmkdemo/WelcomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot Running Inside Docker in Minikube Pod !!";
+        return "Greetings from Spring Boot Running Inside Docker in Minikube Pod !!!";
     }
 }
