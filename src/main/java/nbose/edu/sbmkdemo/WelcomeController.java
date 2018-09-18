23bfb83df4cdcb720555f977576bc14d2a70a075
Nilay K Bose <nilay.bose@gmail.com>
2018-09-18 01:00:20 -0400

    demo

--- src/main/java/nbose/edu/sbmkdemo/WelcomeController.java
+++ src/main/java/nbose/edu/sbmkdemo/WelcomeController.java
@@ -3,6 +3,8 @@ package nbose.edu.sbmkdemo;
 import java.net.InetAddress;
 import java.time.LocalDateTime;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson ;
@@ -10,14 +12,15 @@ import com.google.gson.Gson ;
 @RestController
 public class WelcomeController {
     @RequestMapping( value="/app", produces = "application/json")
-    public String index() {
+    public String index(HttpServletResponse  response) {
+    	response.setHeader("Access-Control-Allow-Origin", "*");
         return new Gson().toJson(new ResponseVO(), ResponseVO.class) ;
     }
-
 }
 
 class ResponseVO{
 	private String msg ; 
+	private String version = "(V1)"; 
 	private String hostname ;
 	private String status = "up" ;
 	private String hostip ;
@@ -28,7 +31,7 @@ class ResponseVO{
 		try { 
 			hostname = InetAddress.getLocalHost().getHostName();
 			hostip   = InetAddress.getLocalHost().getHostAddress();
-			msg = "Greetings (V1) from Spring Boot Running Inside Docker in Minikube Pod !!<br/>" + LocalDateTime.now() ;
+			msg = "Greetings " + version + " from Spring Boot Running Inside Docker in Minikube Pod !!<br/>" + LocalDateTime.now() ;
 		}
 		catch(Exception _ignore) {
 		}
